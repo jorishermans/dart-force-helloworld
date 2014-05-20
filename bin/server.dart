99b3519197a57fcd196669103f48325ac4b85364
joris hermans <JHermans85@hotmail.com>
2014-05-20 12:46:10 +0200

    remove logging code and add entry from framework

--- bin/server.dart
+++ bin/server.dart
@@ -1,27 +1,24 @@
 library chat_example_force;
 
-import 'dart:async';
 import 'dart:io';
-import 'package:logging/logging.dart' show Logger, Level, LogRecord;
 import 'package:force/force_serverside.dart';
 
-final Logger log = new Logger('ChatApp');
-
 void main() {
-  // Set up logger.
-  Logger.root.level = Level.ALL;
-  Logger.root.onRecord.listen((LogRecord rec) {
-    print('${rec.level.name}: ${rec.time}: ${rec.message}');
-  });
+
+    var portEnv = Platform.environment['PORT'];
+    var port = portEnv == null ? 8080 : int.parse(portEnv);
+    var serveClient = portEnv == null ? true : false;
+    
+    // Create a force server
+    ForceServer fs = new ForceServer(port: port, 
+                                     clientFiles: '../build/web/',
+                                     clientServe: serveClient);
+    // Setup logger
+    fs.setupConsoleLog();
+     
+    fs.on('count', (e, sendable) { 
+      sendable.send('notifications', e.json);
+    });
   
-  var portEnv = Platform.environment['PORT'];
-  var port = portEnv == null ? 8080 : int.parse(portEnv);
-  
-  ForceServer fs = new ForceServer(host: "0.0.0.0", port: port, startPage: "index.html" );
-  
-  fs.on('count', (e, sendable) { 
-    sendable.send('notifications', e.json);
-  });
-  
-  fs.start();
+    fs.start();
 }
